4f60740911eb4d822758984e5bfbef5b5fb3eeb8
Alexandr <admin@plin2k.org>
2021-08-08 22:04:04 +0400

    rooms and html

--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -1,65 +1,171 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
+	"time"
 )
+
+type connection struct {
+	ws *websocket.Conn
+	send chan []byte
+}
+
+type hub struct {
+	rooms map[string]map[*connection]bool
+	broadcast chan message
+	register chan subscription
+	unregister chan subscription
+}
+
+type subscription struct {
+	conn *connection
+	room string
+}
+
+type message struct {
+	data []byte
+	room string
+}
+
+var h = hub{
+	broadcast:  make(chan message),
+	register:   make(chan subscription),
+	unregister: make(chan subscription),
+	rooms:      make(map[string]map[*connection]bool),
+}
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
 
-func main() {
-	router := gin.Default()
-	router.LoadHTMLGlob("web/*")
-	router.GET("/", homePage)
-	router.GET("/room/:id", homePage)
-	router.GET("/ws", wsEndpoint)
+var (
+	maxMessageSize int64 = 1024
+	pongWait = time.Hour * 12
+	pingPeriod = time.Second * 5
+	writeWait = time.Second * 10
+)
 
+func main() {
+	go h.run()
+
+	router := gin.New()
+	router.LoadHTMLGlob("./web/*")
+
+	router.GET("/:roomId", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", nil)
+	})
+
+	router.GET("/ws/:roomId", func(c *gin.Context) {
+		roomId := c.Param("roomId")
+		serveWs(c.Writer, c.Request, roomId)
+	})
 	router.Run(":9999")
 }
 
-func homePage(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", nil)
+func serveWs(w http.ResponseWriter, r *http.Request, roomId string) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	c := &connection{send: make(chan []byte, 256), ws: ws}
+	s := subscription{c, roomId}
+	h.register <- s
+	go s.writePump()
+	go s.readPump()
 }
 
-func reader(conn *websocket.Conn) {
+func (s subscription) readPump() {
+	c := s.conn
+	defer func() {
+		h.unregister <- s
+		c.ws.Close()
+	}()
+	c.ws.SetReadLimit(maxMessageSize)
+	c.ws.SetReadDeadline(time.Now().Add(pongWait))
+	c.ws.SetPongHandler(func(string) error { c.ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
 	for {
-		// read in a message
-		messageType, p, err := conn.ReadMessage()
+		_, msg, err := c.ws.ReadMessage()
 		if err != nil {
-			log.Println(err)
-			return
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
+				log.Printf("error: %v", err)
+			}
+			break
 		}
-		// print out that message for clarity
-		fmt.Println(string(p))
-		fmt.Println(messageType)
-
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-
+		m := message{msg, s.room}
+		h.broadcast <- m
 	}
 }
 
-func wsEndpoint(c *gin.Context) {
-	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
-
-	ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
-	if err != nil {
-		log.Println(err)
+func (s *subscription) writePump() {
+	c := s.conn
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.ws.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			if !ok {
+				c.write(websocket.CloseMessage, []byte{})
+				return
+			}
+			log.Println(message)
+			if err := c.write(websocket.TextMessage, message); err != nil {
+				return
+			}
+		case <-ticker.C:
+			if err := c.write(websocket.PingMessage, []byte{}); err != nil {
+				return
+			}
+		}
 	}
+}
 
-	log.Println("Client Connected")
-	err = ws.WriteMessage("id."+string(c.Param("id")), []byte("Hi Client!"))
-	if err != nil {
-		log.Println(err)
+func (c *connection) write(mt int, payload []byte) error {
+	c.ws.SetWriteDeadline(time.Now().Add(writeWait))
+	return c.ws.WriteMessage(mt, payload)
+}
+
+func (h *hub) run() {
+	for {
+		select {
+		case s := <-h.register:
+			connections := h.rooms[s.room]
+			if connections == nil {
+				connections = make(map[*connection]bool)
+				h.rooms[s.room] = connections
+			}
+			h.rooms[s.room][s.conn] = true
+		case s := <-h.unregister:
+			connections := h.rooms[s.room]
+			if connections != nil {
+				if _, ok := connections[s.conn]; ok {
+					delete(connections, s.conn)
+					close(s.conn.send)
+					if len(connections) == 0 {
+						delete(h.rooms, s.room)
+					}
+				}
+			}
+		case m := <-h.broadcast:
+			connections := h.rooms[m.room]
+			for c := range connections {
+				select {
+				case c.send <- m.data:
+				default:
+					close(c.send)
+					delete(connections, c)
+					if len(connections) == 0 {
+						delete(h.rooms, m.room)
+					}
+				}
+			}
+		}
 	}
-
-	reader(ws)
 }
